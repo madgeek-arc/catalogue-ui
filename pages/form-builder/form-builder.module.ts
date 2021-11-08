dec34cbb16544b663797367f7527f1307d2af547
TheQuaker <cs2180008@di.uoa.gr>
2021-11-08 17:01:03 +0200

    form builder first commit

---
+++ pages/form-builder/form-builder.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {FormBuilderComponent} from "./form-builder.component";
+
+@NgModule({
+  declarations: [
+    FormBuilderComponent
+  ],
+  imports: [
+    CommonModule,
+  ],
+})
+
+export class FormBuilderModule {}
