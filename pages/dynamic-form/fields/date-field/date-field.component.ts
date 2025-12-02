a452d32c3026fe3ff43fa9d382ca54076620b159
amantas <48762391+amantas@users.noreply.github.com>
2025-12-02 14:34:19 +0200

    fix: Strip time from ISO strings to handle partial date values

--- pages/dynamic-form/fields/date-field/date-field.component.ts
+++ pages/dynamic-form/fields/date-field/date-field.component.ts
@@ -39,8 +39,6 @@ export class DateFieldComponent extends BaseFieldComponent implements OnInit {
     this.selectedDate = this.parseLocalDate(this.formControl.getRawValue());
   }
 
-
-
   dateChanged(event: Date) {
     if (this.fieldData.typeInfo.values?.[0] === 'formatDateToString') {
       this.formControl.setValue(this.formatDateLocal(event));
@@ -81,6 +79,10 @@ export class DateFieldComponent extends BaseFieldComponent implements OnInit {
       return new Date(Number(value));
     }
 
+    // strip time from ISO strings to handle partial date values
+    if(value.includes('T')){
+      value = value.split('T')[0];
+    }
     // 3. Handle partial date: "YYYY-MM-DD"
     if (/^\d{4}-\d{2}-\d{2}$/.test(value)) {
       const [year, month, day] = value.split('-').map(Number);
